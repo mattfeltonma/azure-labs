4c00c710f3a18daac5a62c20781b14115330d681
Matt Felton <matt.felton@feltonma.com>
2021-04-07 17:35:18 -0400

    Modified - Corrected custom script extension version for linux and tweaked ps script

--- simple-landing-zone/scripts/prepare_windows_vm.ps1
+++ simple-landing-zone/scripts/prepare_windows_vm.ps1
@@ -1,21 +1,48 @@
-# Install RSAT Tools
-Install-WindowsFeature -IncludeAllSubFeature RSAT
-
-# Download SMSS
 Start-Transcript -path "C:\MachinePrep\logs\log.txt"
-$url = "https://aka.ms/ssmsfullsetup"
-$destination = "C:\MachinePrep\files\smss.exe"
-Invoke-WebRequest -Uri $url -OutFile $destination
 
-# Install SMSS
-$params = " /Install /Quiet /Norestart /Logs C:\MachinePrep\logs\smss_install.log"
-$params_formatted = $params.Split(" ")
-& "$destination" $params_formatted | Out-Null
+try {
+    # Install RSAT Tools
+    Write-Host "Installing RSAT Tools..."
+    Install-WindowsFeature -IncludeAllSubFeature RSAT
+}
+catch {
+    "Unable to install RSAT Tools"
+}
 
-# Install Azure CLI
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+try {
+    # Download SMSS
+    Write-Host "Downloading SMSS..."
+    New-Item -ItemType directory -Path C:\MachinePrep\files
+    $url = "https://aka.ms/ssmsfullsetup"
+    $destination = "C:\MachinePrep\files\smss.exe"
+    Invoke-WebRequest -Uri $url -OutFile $destination
 
-# Install Azure PowerShell
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+    # Install SMSS
+    Write-Host "Installing SMSS..."
+    $params = " /Install /Quiet /Norestart /Logs C:\MachinePrep\logs\smss_install.log"
+    $params_formatted = $params.Split(" ")
+    & "$destination" $params_formatted | Out-Null
+}
+catch {
+    "Unable to install SMSS"
+}
 
-Stop-Transcript
+try {
+
+    # Install nuget package manager
+    Write-Host "Instaling nuget..."
+    Install-PackageProvider -Name Nuget -Force 
+
+    # Install Azure CLI
+    Write-Host "Downloading Azure CLI..."
+    Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
+    # Install Azure PowerShell
+    Write-Host "Installing Azure CLI..."
+    Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+}
+catch {
+    "Unable to install Azure CLI"
+}
+
+Stop-Transcript 
