31ef7985b4cdf78e03518d755fd9fbbf91b69061
Matt Felton <matt.felton@feltonma.com>
2021-04-08 18:29:44 -0400

    Modified - Corrected shell command

--- simple-landing-zone/scripts/prepare_ubuntu_vm.sh
+++ simple-landing-zone/scripts/prepare_ubuntu_vm.sh
@@ -2,8 +2,7 @@
 
 # Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb -o azurecli.sh
-pwd
-sudo ./azurecli.sh
+sudo sh azurecli.sh
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
