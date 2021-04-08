7ec02038edcd718e86a9d7b099e3aa61528f0e2a
Matt Felton <matt.felton@feltonma.com>
2021-04-08 18:40:52 -0400

    Modified - Modified shell script to install with package management

--- simple-landing-zone/scripts/prepare_ubuntu_vm.sh
+++ simple-landing-zone/scripts/prepare_ubuntu_vm.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 # Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb -o azurecli.sh
-chmod 700 ./azurecli.sh
-sudo ./azurecli.sh
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
