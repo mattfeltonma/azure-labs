9d796ceaed243dc8891650e5be7697221dcbf9f2
Matt Felton <matt.felton@feltonma.com>
2021-04-15 11:47:09 -0400

    Modified - Edited Ubuntu post-installation script to install Docker

--- simple-landing-zone/scripts/prepare_ubuntu_vm.sh
+++ simple-landing-zone/scripts/prepare_ubuntu_vm.sh
@@ -15,3 +15,12 @@ sudo apt-get install azure-cli
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install Docker
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $1
