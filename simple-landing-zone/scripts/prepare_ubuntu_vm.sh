296ef3b1bbb1613b98af8907e8ae9a5264081ba7
Matt Felton <matt.felton@feltonma.com>
2021-04-16 08:11:31 -0400

    Modified - Corrected package installation to force acceptance

--- simple-landing-zone/scripts/prepare_ubuntu_vm.sh
+++ simple-landing-zone/scripts/prepare_ubuntu_vm.sh
@@ -2,7 +2,7 @@
 
 # Install Azure CLI
 sudo apt-get update
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+sudo apt-get -y install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -10,20 +10,20 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get -y install azure-cli
 
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 # Install Docker
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $1
 
 # Install Expect
-sudo apt-get install expect
+sudo apt-get -y install expect
