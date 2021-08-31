7983e8b534ab5ef77aaf827c93986dacda675ae2
Matt Felton <matt.felton@feltonma.com>
2021-08-31 19:56:04 -0400

    Modified - Simplified deployment script

--- app-gw-app-service-pe/scripts/config-agw.sh
+++ app-gw-app-service-pe/scripts/config-agw.sh
@@ -65,43 +65,10 @@ AGW_CERT_SECRET_ID=$(az keyvault certificate show \
 --query=sid \
 --output=tsv)
 
-# Set the cert in App Gateway
-az network application-gateway ssl-cert create \
---resource-group=$TRANSIT_RG \
---gateway-name=$AGW_NAME \
---name=$AGW_CERT_NAME \
---key-vault-secret-id=$AGW_CERT_SECRET_ID
-
-# Configure the https front end port
-az network application-gateway frontend-port create \
---gateway-name=$AGW_NAME \
---name=https_port \
---port=443 \
---resource-group=$TRANSIT_RG
-
-# Configure the https listener
-az network application-gateway http-listener create \
---name=https_listener \
---frontend-ip=appGwPubFrontEnd \
---frontend-port=443 \
---gateway-name=$AGW_NAME \
---resource-group=$TRANSIT_RG \
---frontend-port=https_port \
---host-names=$DOMAIN_NAME \
---ssl-cert=$AGW_CERT_NAME
-
-# Create a new address pool
-az network application-gateway address-pool create \
---gateway-name=$AGW_NAME \
---name=sample-app-pool \
---resource-group=$TRANSIT_RG \
---servers=$APP_FQDN
-
-# Create a new routing rule
-az network application-gateway rule create \
---gateway-name=$AGW_NAME \
---name=https_rule \
---resource-group=$TRANSIT_RG \
---address-pool=sample-app-pool \
---http-listener=https_listener \
---http-settings=https_httpsetting
+# Write the result to output
+RESULT=$(cat << EOF
+{
+  "CERT_SECRET": "$AGW_CERT_SECRET_ID"
+}
+EOF
+)
