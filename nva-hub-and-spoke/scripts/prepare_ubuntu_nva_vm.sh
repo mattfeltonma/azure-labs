e746b33309e579d026d7e7dd21dfe19b84a32618
Matt Felton <matt.felton@feltonma.com>
2022-11-20 15:26:31 -0500

    Modified - Added Quagga config
    Added Quagga to NVA build.

--- nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
+++ nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# Update repositories
+#   Update repositories
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 sleep 30
 
-# Disable built-in firewall. Management will be done direct with iptables
+#   Disable built-in firewall. Management will be done direct with iptables
 ufw disable
 
-# Install tools
+#   Install net tools
 apt-get -o DPkg::Lock::Timeout=30 install net-tools -y
 
-# Install support for persistency to iptables
+#   Install Quagga
+apt-get -o DPkg::Lock::Timeout=30 install quagga -y
+
+#   Install support for persistency to iptables
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get -o DPkg::Lock::Timeout=30 install iptables-persistent -y
 
 
-# Add kernal modules to support vrfs
+#   Add kernal modules to support vrfs
 apt-get install -o DPkg::Lock::Timeout=30 linux-modules-extra-azure -y
 
-# Enable IPv4 forwarding
+#   Enable IPv4 forwarding
 sed -r -i 's/#{1,}?net.ipv4.ip_forward ?= ?(0|1)/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 
-# Enable vrf support in network stacks
+#   Enable vrf support in network stacks
 sysctl -p | grep -i "net.ipv4.tcp_l3"
 if [ $? -eq 1 ]
 then
@@ -32,7 +35,7 @@ then
     sysctl -p
 fi
 
-# Configure routing
+#   Configure routing
 ls -l /etc/systemd/system/routingconfig.service
 if [ $? -eq 2 ]
 then
@@ -64,6 +67,80 @@ EOF
     systemctl daemon-reload
     systemctl start routingconfig.service
     systemctl enable routingconfig.service
+fi
+
+#   Configure Quagga
+ls -l /var/log/zebra.log > /dev/null
+if [ $? -eq 2 ]
+then
+    mkdir -p /var/log/quagga && sudo chown quagga:quagga /var/log/quagga
+    touch /var/log/zebra.log
+    chown quagga:quagga /var/log/zebra.log
+    touch /etc/quagga/babeld.conf
+    touch /etc/quagga/bgpd.conf
+    touch /etc/quagga/isisd.conf
+    touch /etc/quagga/ospf6d.conf
+    touch /etc/quagga/ospfd.conf
+    touch /etc/quagga/ripd.conf
+    touch /etc/quagga/ripngd.conf
+    touch /etc/quagga/vtysh.conf
+    touch /etc/quagga/zebra.conf
+    chown quagga:quagga /etc/quagga/babeld.conf && sudo chmod 640 /etc/quagga/babeld.conf
+    chown quagga:quagga /etc/quagga/bgpd.conf && sudo chmod 640 /etc/quagga/bgpd.conf
+    chown quagga:quagga /etc/quagga/isisd.conf && sudo chmod 640 /etc/quagga/isisd.conf
+    chown quagga:quagga /etc/quagga/ospf6d.conf && sudo chmod 640 /etc/quagga/ospf6d.conf
+    chown quagga:quagga /etc/quagga/ospfd.conf && sudo chmod 640 /etc/quagga/ospfd.conf
+    chown quagga:quagga /etc/quagga/ripd.conf && sudo chmod 640 /etc/quagga/ripd.conf
+    chown quagga:quagga /etc/quagga/ripngd.conf && sudo chmod 640 /etc/quagga/ripngd.conf
+    chown quagga:quaggavty /etc/quagga/vtysh.conf && sudo chmod 660 /etc/quagga/vtysh.conf
+    chown quagga:quagga /etc/quagga/zebra.conf && sudo chmod 640 /etc/quagga/zebra.conf
+    echo 'zebra=yes' > /etc/quagga/daemons
+    echo 'bgpd=yes' >> /etc/quagga/daemons
+    echo 'ospfd=no' >> /etc/quagga/daemons
+    echo 'ospf6d=no' >> /etc/quagga/daemons
+    echo 'ripd=no' >> /etc/quagga/daemons
+    echo 'ripngd=no' >> /etc/quagga/daemons
+    echo 'isisd=no' >> /etc/quagga/daemons
+    echo 'babeld=no' >> /etc/quagga/daemons
+    echo "add zebra config"
+    
+# # Create zebra config file
+    cat <<EOF > /etc/quagga/zebra.conf
+!
+interface eth1
+!
+interface lo
+!
+table 10
+!
+ip forwarding
+!
+line vty
+!
+EOF
+
+# # Create bgpd config file
+    cat <<EOF > /etc/quagga/bgpd.conf
+!
+router bgp $6
+ bgp router-id $5
+!
+ address-family ipv6
+ exit-address-family
+ exit
+!
+line vty
+!
+EOF
+# # Configure Quagga to run in the vrf
+    sed -r -i 's/\/usr\/sbin\/bgpd -d -A 127\.0\.0\.1 -f \/etc\/quagga\/bgpd\.conf/ip vrf exec vrflan \/usr\/sbin\/bgpd -d -A 127\.0\.0\.1 -f \/etc\/quagga\/bgpd\.conf/g' /lib/systemd/system/bgpd.service
+
+# # Start Quagga services
+    systemctl daemon-reload
+    systemctl enable zebra.service
+    systemctl enable bgpd.service
+    systemctl start zebra 
+    systemctl start bgpd  
 fi
 
 #   Configure iptables
