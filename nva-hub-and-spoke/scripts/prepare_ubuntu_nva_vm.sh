7bf4e94d5305529f385987df2b9dc7120c248165
Matt Felton <matt.felton@feltonma.com>
2022-11-20 17:21:30 -0500

    modified - extended sleep

--- nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
+++ nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
@@ -2,7 +2,7 @@
 
 #   Update repositories
 export DEBIAN_FRONTEND=noninteractive
-apt-get -o DPkg::Lock::Timeout=60 update && sleep 15
+apt-get -o DPkg::Lock::Timeout=60 update && sleep 30
 
 #   Disable built-in firewall. Management will be done direct with iptables
 ufw disable
