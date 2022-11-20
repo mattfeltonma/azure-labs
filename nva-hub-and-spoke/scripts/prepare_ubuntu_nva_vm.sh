4a51c2b626d0d9ca5ff3742877e36358ebb44f61
Matt Felton <matt.felton@feltonma.com>
2022-11-20 16:41:15 -0500

    Modified - Added wait before apt update
    Added wait to troubleshoot issue where apt install
    was running before apt update finished

--- nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
+++ nva-hub-and-spoke/scripts/prepare_ubuntu_nva_vm.sh
@@ -2,7 +2,7 @@
 
 #   Update repositories
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
+apt-get -o DPkg::Lock::Timeout=60 update
 sleep 30
 
 #   Disable built-in firewall. Management will be done direct with iptables
