918893b1892e114e9ba1f0a0ef101df596b9e9b6
Matt Felton <matt.felton@feltonma.com>
2022-11-30 21:15:07 -0500

    modified - initial commit

---
+++ multi-region-vwan-simple/dsc/windowsdevvm.ps1
@@ -0,0 +1,47 @@
+configuration WindowsDevVM {
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$MachineName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory=$false)]
+        [string]$SystemTimeZone = "Eastern Standard Time",
+
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
+    )
+
+    Import-DscResource -ModuleName xStorage, xNetworking, ComputerManagementDsc, PSDesiredStateConfiguration
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    $InterfaceAlias = $($Interface.Name)
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        TimeZone TimeZoneSet
+        {
+            IsSingleinstance = 'Yes'
+            TimeZone = $SystemTimeZone
+        }
+
+        IEEnhancedSecurityConfiguration 'DisableForAdministrators'
+        {
+            Role = 'Administrators'
+            Enabled = $false
+        }
+
+        IEEnhancedSecurityConfiguration 'DisableForUsers'
+        {
+            Role    = 'Users'
+            Enabled = $false
+        }
+    }
+}
